5fca81bb160f6fd1d79fb5854cffb157650a6060
YaseenFsharid <yaseen.sharid@gmail.com>
2020-02-17 17:03:10 +0200

    finished lab 2

---
+++ js/app.js
@@ -0,0 +1,75 @@
+'use strict';
+var name = prompt("please enter your name");
+alert("hello "+name+" please answer with yes or no or y/n"); 
+var name2 = prompt("is my name yaseen sharid ?");
+console.log(name2);
+
+console.log(name2);
+switch (name2.toUpperCase){
+case 'YES':
+    console.log(name2);
+case 'Y':
+console.log(name2);
+break;
+case 'NO':
+    console.log(name2);
+case 'N':
+console.log(name2);
+break;
+}
+var career = prompt("did i study in hashemite unevirsty ?");
+switch(career.toLowerCase()){
+case 'yes':
+    console.log(career);
+case 'y':
+console.log(career);
+break;
+case 'no':
+    console.log(career);
+case 'n':
+console.log(career);
+break;
+}
+var paint = prompt("did i painting the names with thier stories ?");
+switch (paint.toLowerCase()){
+case 'yes':
+    console.log(paint);
+case 'y':
+console.log(paint);
+break;
+case 'no':
+    console.log(paint);
+case 'n':
+console.log(paint);
+break;
+}
+var hardwork = prompt("do i had an experiance of constructing and painting wall" );
+
+switch (hardwork.toLowerCase()){
+case 'yes':
+    console.log(hardwork);
+case 'y':
+console.log(hardwork);
+break;
+case 'no':
+    console.log(hardwork);
+case 'n':
+console.log(hardwork);
+break;
+}
+var run = prompt("do i like to run daily morning?");
+switch (run.toLowerCase()){
+case 'yes':
+    console.log(run);
+case 'y':
+console.log(run);
+break;
+case 'no':
+    console.log(run);
+case 'n':
+console.log(run);
+break;
+}
+alert("welcom "+ name + "thanks for doing that" ); 
+
+
