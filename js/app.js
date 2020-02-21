95e4f5d4a29e8519095605c4d8276aa726140337
YaseenFsharid <yaseen.sharid@gmail.com>
2020-02-21 13:51:39 +0200

    adding some staff

--- js/app.js
+++ js/app.js
@@ -3,87 +3,102 @@ function guessGame() {
     var name = prompt("please enter your name");
     alert("hello " + name + " please answer with yes or no or y/n");
     var name2 = prompt("is my name yaseen sharid ?");
-    console.log(name2);
+
     var rightAnswer = 0;
-    console.log(name2);
+
     switch (name2.toUpperCase) {
         case 'YES':
-            console.log(name2);
+            alert("this is a right answer" + name2);
             rightAnswer += 1;
+            break;
         case 'Y':
-            console.log(name2);
+            alert("this is a right answer" + name2);
             rightAnswer += 1;
             break;
         case 'NO':
-            console.log(name2);
+            alert("this is a wrong answer "+name2);
+            break;
         case 'N':
-            console.log(name2);
+            alert("this is a wrong answer "+name2);
             break;
     }
+    document.getElementById("1").innerHTML = "your first answer is : "+ name2;
     var career = prompt("did i study in hashemite unevirsty ?");
     switch (career.toLowerCase()) {
         case 'yes':
-            console.log(career);
+            alert("this is a right answer" +career);
             rightAnswer += 1;
+            break;
         case 'y':
-            console.log(career);
+            alert("this is a right answer" +career);
             rightAnswer += 1;
             break;
         case 'no':
-            console.log(career);
+            alert("this is a wrong answer "+career);
+            break;
         case 'n':
-            console.log(career);
+            alert("this is a wrong answer "+career);
             break;
     }
+    document.getElementById("2").innerHTML = "your seconed answer is : "+ career;
     var paint = prompt("did i painting the names with thier stories ?");
     switch (paint.toLowerCase()) {
         case 'yes':
-            console.log(paint);
+            alert("this is a right answer" +paint);
             rightAnswer += 1;
+            break;
         case 'y':
-            console.log(paint);
             rightAnswer += 1;
+            alert("this is a right answer" +paint);
             break;
         case 'no':
-            console.log(paint);
+            alert("this is a wrong answer "+paint);
+            break;
         case 'n':
-            console.log(paint);
+            alert("this is a wrong answer "+paint);
             break;
     }
+    document.getElementById("3").innerHTML = "your third answer is : "+ paint;
     var hardwork = prompt("do i had an experiance of constructing and painting wall");
 
     switch (hardwork.toLowerCase()) {
         case 'yes':
-            console.log(hardwork);
+            alert("this is a right answer" +hardwork);
             rightAnswer += 1;
+            break;
         case 'y':
-            console.log(hardwork);
+            alert("this is a right answer" +hardwork);
             rightAnswer += 1;
             break;
         case 'no':
-            console.log(hardwork);
+            alert("this is a wrong answer "+hardwork);
+            break;
         case 'n':
-            console.log(hardwork);
+            alert("this is a wrong answer "+hardwork);
             break;
     }
+    document.getElementById("4").innerHTML = "your forth answer is : "+ hardwork;
     var run = prompt("do i like to run daily morning?");
     switch (run.toLowerCase()) {
         case 'yes':
-            console.log(run);
+            alert("this is a right answer" +run);
             rightAnswer += 1;
+            break;
         case 'y':
-            console.log(run);
+            alert("this is a right answer" +run);
             rightAnswer += 1;
             break;
         case 'no':
-            console.log(run);
+            alert("this is a wrong answer "+run);
+            break;
         case 'n':
-            console.log(run);
+            alert("this is a wrong answer "+run);
             break;
     }
+    document.getElementById("5").innerHTML = "your fifth answer is : "+ run;
     alert("welcom " + name + "thanks for doing that");
 
-
+    
     var numOFAttemps = 0;
     var myNum = 3;
     var num = Number(prompt("please enter a number between 0 and 5"));
@@ -91,6 +106,7 @@ function guessGame() {
     while (numOFAttemps < 4) {
         if (num == myNum) {
             alert("awsome you guess my number");
+            var ans =num;
             rightAnswer += 1;
             break;
         } else if (num < myNum) {
@@ -107,23 +123,25 @@ function guessGame() {
             alert("my number is 3");
         }
     }
-
+    document.getElementById("6").innerHTML = "your sexth answer is : "+ "this is a right answer" + ans;
+    
     var smokings = ["winston", "marlboro", "kent", "gelouas", "captin black", "lm", "lm loft", "Ld", "kent advance", "marlboro touch"];
-    var mySmoke = smokings[2];
+    
 
     var attemps = 0;
-
-
-
 
     for (var i = 0; i < 6; i++) {
         var gsSmoke = prompt("can you guess what kind of smoking i smoke ?");
         for (var j = 0; j < smokings.length; j++) {
             if (gsSmoke == smokings[j]) {
-                alert("awsome you've guess my smoking");
-                rightAnswer += 1;
-                attemps += 1;
-                break;
+              alert("awsome you've guess my smoking");
+              var sm = gsSmoke;
+              document.getElementById("7").innerHTML = "your seventh answer is : "+ "this is a right answer" + sm;  
+              rightAnswer += 1;
+              
+              attemps += 1;
+              document.getElementById("8").innerHTML="Your score is : " + rightAnswer; 
+              break;
 
 
             }
@@ -134,7 +152,19 @@ function guessGame() {
         }
 
     }
+    
     return alert("your score is :" + rightAnswer + " good job .");
+     
+
+ 
 }
 
 guessGame();
+
+
+
+
+
+
+
+
